723e9eeaac2eb909f2f635e8b7104d2896ce491a
arunain123 <arunainm@gmail.com>
2024-03-07 12:11:35 +0530

    Created header and footer

---
+++ ibe-team7-frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import IBE from "./components/IBE/IBE";
+import "./App.css";
+function App() {
+  return (
+    <div className="app">
+      <IBE />
+    </div>
+  );
+}
+
+export default App;
---
+++ ibe-team7-frontend/src/components/header/Header.tsx
@@ -0,0 +1,22 @@
+import "../../styling/Header.scss"; // Import the CSS file
+import img1 from "../../assets/enicon.png";
+import img2 from "../../assets/USDIcon.png";
+export function Header() {
+  return (
+    <div className="header">
+      <div className="header-left">
+        <strong>Kickdrum</strong> Internet Booking Engine
+      </div>
+      <div className="header-right">
+        <div className="img1">
+          <img src={img1} alt="EN" />
+          <span className="en-text">en</span>
+        </div>
+        <div className="img2">
+          <img src={img2} alt="USD" />
+          <span className="USD-text">USD</span>
+        </div>
+      </div>
+    </div>
+  );
+}
