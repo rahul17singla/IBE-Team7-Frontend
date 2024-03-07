f51117ef3dd52e43beaf54e549d1a2458f41497d
prashant1shukla <prashantshukla@gmail.com>
2024-03-07 18:37:32 +0530

    Completed the Fronted testing and Multi-env setup

--- ibe-team7-frontend/src/App.tsx
+++ ibe-team7-frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import IBE from "./components/IBE/IBE";
 import "./App.css";
 function App() {
+  console.log(import.meta.env.VITE_REACT_APP_ENV);
   return (
     <div className="app">
       <IBE />
--- ibe-team7-frontend/src/components/main/Main.tsx
+++ ibe-team7-frontend/src/components/main/Main.tsx
@@ -1,7 +1,20 @@
 import "../../styling/Main.scss";
 
 export function Main() {
-  return <div className="main">
-    <button onClick={() => methodDoesNotExist()} className="btn">Break the world</button>;
-  </div>;
+  const env = import.meta.env.VITE_REACT_APP_ENV;
+
+  // Define a method that intentionally throws an error
+  const methodThatThrowsError = () => {
+    throw new Error('This is an intentional error!');
+  };
+
+  return (
+    <div className="main">
+      <p>Welcome, {env}</p>
+      {/* Call the methodThatThrowsError function */}
+      <button onClick={methodThatThrowsError} className="btn">
+        Break the world
+      </button>
+    </div>
+  );
 }
