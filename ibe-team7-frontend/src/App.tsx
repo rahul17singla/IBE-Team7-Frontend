5a2f42f0cdd05d53b8e645b649492f0bde47bcc3
rahul17singla <rahulsingla1710@gmail.com>
2024-03-08 12:20:32 +0530

    header and footer made responsive

--- ibe-team7-frontend/src/App.tsx
+++ ibe-team7-frontend/src/App.tsx
@@ -1,12 +1,21 @@
-import IBE from "./components/IBE/IBE";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Header } from "./components/header/Header";
+import { Footer } from "./components/footer/Footer";
+import { Home } from "./components/home/Home";
 function App() {
-  console.log(import.meta.env.VITE_REACT_APP_ENV);
-  return (
-    <div className="app">
-      <IBE />
-    </div>
-  );
+    console.log(import.meta.env.VITE_REACT_APP_ENV);
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+                <Footer />
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
