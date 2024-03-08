fd3914d8817981745258ec564eb86326650c4006
arunain123 <arunainm@gmail.com>
2024-03-08 16:57:00 +0530

    Language and Currency Conversion

--- ibe-team7-frontend/src/components/header/Header.tsx
+++ ibe-team7-frontend/src/components/header/Header.tsx
@@ -2,7 +2,17 @@ import "./Header.scss";
 import language from "../../assets/enicon.png";
 import currency from "../../assets/USDIcon.png";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import  { ChangeEvent } from "react";
+
 export function Header() {
+    const { t, i18n } = useTranslation();
+
+    const handleLanguageChange = (event:ChangeEvent<HTMLSelectElement>) => {
+        const selectedLanguage = event.target.value;
+        i18n.changeLanguage(selectedLanguage);
+    };
+
     return (
         <div className="header">
             <div className="header-left">
@@ -17,15 +27,20 @@ export function Header() {
                     Kickdrum
                 </Link>
                 <Link to={"/"} className="header-left__text">
-                    Internet Booking Engine
+                Internet Booking Engine
                 </Link>
             </div>
             <div className="header-right">
                 <button className="language">
                     <img src={language} alt="EN" />
-                    <select className="language" name="language" id="language">
-                        <option value="En">En</option>
-                        <option value="Spa">Spa</option>
+                    <select
+                        className="language"
+                        name="language"
+                        id="language"
+                        onChange={handleLanguageChange}
+                    >
+                        <option value="en">En</option>
+                        <option value="fr">Fr</option>
                     </select>
                 </button>
                 <button className="currency">
