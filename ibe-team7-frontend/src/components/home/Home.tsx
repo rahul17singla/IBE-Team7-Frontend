3cede588911e5671b3730bbaa3a3bfa2b1826f5a
arunain123 <arunainm@gmail.com>
2024-03-08 16:57:00 +0530

    Language and Currency Conversion

--- ibe-team7-frontend/src/components/home/Home.tsx
+++ ibe-team7-frontend/src/components/home/Home.tsx
@@ -1,20 +1,41 @@
 import "./Home.scss";
 
+import { CurrencyConverter } from "../currency-converter/CurrencyConverter";
+import LanguageConversion  from "../language-conversion/LanguageConversion";
+
 export function Home() {
-    const env = import.meta.env.VITE_REACT_APP_ENV;
+  const env = import.meta.env.VITE_REACT_APP_ENV;
 
-    // Define a method that intentionally throws an error
-    const methodThatThrowsError = () => {
-        throw new Error("This is an intentional error!");
-    };
+  // Define a method that intentionally throws an error
+  const methodThatThrowsError = () => {
+    throw new Error("This is an intentional error!");
+  };
 
-    return (
-        <div className="main">
-            <p>Welcome, {env}</p>
+
+
+  return (
+    <div className="main">
+        <div>
+                <p>Welcome, {env}</p>
             {/* Call the methodThatThrowsError function */}
             <button onClick={methodThatThrowsError} className="btn">
                 Break the world
             </button>
         </div>
-    );
+     
+
+ 
+     
+      <div>
+         
+       <LanguageConversion/>
+
+      </div>
+       
+      
+      <div>
+        <CurrencyConverter />
+      </div>
+    </div>
+  );
 }
