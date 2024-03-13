13de8e4f71b07e8ce219bc2b58dc9036c86b9043
rahul17singla <rahulsingla1710@gmail.com>
2024-03-13 11:28:15 +0530

    resolved pull request problems

--- ibe-team7-frontend/src/components/currency-converter/CurrencyConverter.tsx
+++ ibe-team7-frontend/src/components/currency-converter/CurrencyConverter.tsx
@@ -1,111 +1,110 @@
-import  { useState, useEffect } from "react";
-import CurrencyInput from "react-currency-input-field";
+import { useState, useEffect } from "react";
 import "./CurrencyConverter.scss";
-
-interface Rates {
-  [key: string]: number;
-}
-
+import { Rates } from "../../types/Rates";
 
 export function CurrencyConverter() {
-  const [rates, setRates] = useState<Rates | undefined>();
-  const [ratesFetched, setRatesFetched] = useState<boolean>(false);
-  const [amount, setAmount] = useState<number>(0);
-  const [fromCurrency, setFromCurrency] = useState<string>("USD");
-  const [toCurrency, setToCurrency] = useState<string>("USD");
-  const [output, setOutput] = useState<number | undefined>();
+    const [rates, setRates] = useState<Rates | undefined>();
+    const [ratesFetched, setRatesFetched] = useState<boolean>(false);
+    const [amount, setAmount] = useState<number>(0);
+    const [fromCurrency, setFromCurrency] = useState<string>("USD");
+    const [toCurrency, setToCurrency] = useState<string>("USD");
+    const [output, setOutput] = useState<number | undefined>();
 
-  const getRates = async () => {
-    // fetch the data from API
-    const response = await fetch(
-      " https://v6.exchangerate-api.com/v6/f4c2fbc9af7f2297f39b0297/latest/USD"
-    ).then((response) => response.json());
+    const getRates = async () => {
+        // fetch the data from API
+        const response = await fetch(
+            "https://v6.exchangerate-api.com/v6/f4c2fbc9af7f2297f39b0297/latest/USD"
+        ).then((response) => response.json());
 
-    // save the rates in the state
-    if (response.result === "success") {
-      setRates(response.conversion_rates);
-      setRatesFetched(true);
-    }
-  };
+        // save the rates in the state
+        if (response.result === "success") {
+            setRates(response.conversion_rates);
+            setRatesFetched(true);
+        }
+    };
 
-  useEffect(() => {
-    getRates();
-  }, []);
+    useEffect(() => {
+        getRates();
+    }, []);
 
-  const calculateOutput = async () => {
-    if (!rates) return;
+    const calculateOutput = async () => {
+        if (!rates) return;
 
-    // fetch the selected from currency rates
-    const response = await fetch(
-      ` https://v6.exchangerate-api.com/v6/f4c2fbc9af7f2297f39b0297/latest/${fromCurrency}`
-    ).then((response) => response.json());
-    const fetchedRates = response.conversion_rates;
-    const currencyRate = fetchedRates[toCurrency];
-    const outputValue = amount * currencyRate;
-    setOutput(outputValue);
-  };
+        // fetch the selected from currency rates
+        const response = await fetch(
+            `https://v6.exchangerate-api.com/v6/f4c2fbc9af7f2297f39b0297/latest/${fromCurrency}`
+        ).then((response) => response.json());
+        const fetchedRates = response.conversion_rates;
+        const currencyRate = fetchedRates[toCurrency];
+        const outputValue = amount * currencyRate;
+        setOutput(outputValue);
+    };
 
-  return (
-    <div className="container">
-      <div className="input-amount">
-        <label>Amount:</label>
-        <CurrencyInput
-  value={amount}
-  onValueChange={(amountValue) =>
-    setAmount(amountValue !== undefined ? parseFloat(amountValue) : 0)
-  }
-  intlConfig={{ locale: "en-US", currency: fromCurrency }}
-  allowDecimals={true}
-  allowNegativeValue={false}
-/>
+    return (
+        <div className="container">
+            {/* <div className="input-amount">
+                <label>Amount:</label>
+                <CurrencyInput
+                    value={amount}
+                    onValueChange={(amountValue) =>
+                        setAmount(
+                            amountValue !== undefined
+                                ? parseFloat(amountValue)
+                                : 0
+                        )
+                    }
+                    intlConfig={{ locale: "en-US", currency: fromCurrency }}
+                    allowDecimals={true}
+                    allowNegativeValue={false}
+                />
+                </div>
+            */}
 
-      </div>
+            {/*
+            <div className="input-from">
+                <label>From:</label>
+                <select
+                    id="from"
+                    value={fromCurrency}
+                    onChange={(e) => setFromCurrency(e.target.value)}
+                >
+                    {ratesFetched ? (
+                        Object.keys(rates!).map((currency, index) => (
+                            <option key={index} value={currency}>
+                                {currency}
+                            </option>
+                        ))
+                    ) : (
+                        <option>USD</option>
+                    )}
+                </select>
+            </div> 
+            */}
 
-      <div className="input-from">
-        <label>From:</label>
-        <select
-          id="from"
-          value={fromCurrency}
-          onChange={(e) => setFromCurrency(e.target.value)}
-        >
-          {ratesFetched ? (
-            Object.keys(rates!).map((currency, index) => (
-              <option key={index} value={currency}>
-                {currency}
-              </option>
-            ))
-          ) : (
-            <option>USD</option>
-          )}
-        </select>
-      </div>
-
-      <div className="input-to">
-        <label>To:</label>
-        <select
-          id="to"
-          value={toCurrency}
-          onChange={(e) => setToCurrency(e.target.value)}
-        >
-          {ratesFetched ? (
-            Object.keys(rates!).map((currency, index) => (
-              <option key={index} value={currency}>
-                {currency}
-              </option>
-            ))
-          ) : (
-            <option>EUR</option>
-          )}
-        </select>
-      </div>
-      <button className="btn" onClick={() => calculateOutput()}>
-        Calculate
-      </button>
-      <div className="output">
-        <label>Output: {output}</label>
-      </div>
-    </div>
-  );
+            {/* <div className="input-to">
+                <label>To:</label>
+                <select
+                    id="to"
+                    value={toCurrency}
+                    onChange={(e) => setToCurrency(e.target.value)}
+                >
+                    {ratesFetched ? (
+                        Object.keys(rates!).map((currency, index) => (
+                            <option key={index} value={currency}>
+                                {currency}
+                            </option>
+                        ))
+                    ) : (
+                        <option>EUR</option>
+                    )}
+                </select>
+            </div> */}
+            <button className="btn" onClick={() => calculateOutput()}>
+                Calculate
+            </button>
+            <div className="output">
+                <label>Output: {output}</label>
+            </div>
+        </div>
+    );
 }
-
-
