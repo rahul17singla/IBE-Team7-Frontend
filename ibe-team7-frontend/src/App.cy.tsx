5b8fed72c3a7036ea2c30901236817dd099e4996
rahul17singla <rahulsingla1710@gmail.com>
2024-03-17 11:36:44 +0530

    minor changes for amplify

--- ibe-team7-frontend/src/App.cy.tsx
+++ ibe-team7-frontend/src/App.cy.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import App from './App'
+import App from "./App";
 
-describe('<App />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<App />)
-  })
-})
+describe("<App />", () => {
+    it("renders", () => {
+        // see: https://on.cypress.io/mounting-react
+        cy.mount(<App />);
+    });
+});
