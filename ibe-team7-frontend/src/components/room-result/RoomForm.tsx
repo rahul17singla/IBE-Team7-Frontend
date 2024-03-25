2325729ff5481e53cfe19e1658ffaf17361af44c
rahul17singla <rahulsingla1710@gmail.com>
2024-03-25 14:36:32 +0530

    backend url changed

--- ibe-team7-frontend/src/components/room-result/RoomForm.tsx
+++ ibe-team7-frontend/src/components/room-result/RoomForm.tsx
@@ -87,6 +87,10 @@ export const RoomForm = () => {
     };
 
     useEffect(() => {
+        const url = window.location.href;
+        if (!url.includes("?")) {
+            navigate("/");
+        }
         const fetchData = async () => {
             console.log(params);
             if (params.has("adults")) {
