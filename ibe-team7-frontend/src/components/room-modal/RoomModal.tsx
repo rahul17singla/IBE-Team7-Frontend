bb806fdcf7844f7c615ea34e0119b092a2527b9a
arunain123 <arunainm@gmail.com>
2024-03-28 11:54:37 +0530

    added modal page

--- ibe-team7-frontend/src/components/room-modal/RoomModal.tsx
+++ ibe-team7-frontend/src/components/room-modal/RoomModal.tsx
@@ -1,5 +1,296 @@
-import "./RoomModal.scss";
 
-export const RoomModal = ({ room }: any) => {
-    return <div className="modal-container">{room.roomTypeName}</div>;
-};
+
+
+// import { useEffect, useState } from "react";
+// import { CarouselComponent } from "../room-result/card/CarouselComponent";
+// import "./RoomModal.scss";
+// import axios from "axios";
+// import { BACKEND_URL } from "../../constants/Constants";
+// import Carousel from "react-material-ui-carousel";
+// import User from "../../assets/user.svg";
+// import Doublebed from "../../assets/doublebed.svg";
+
+// const amenitiesList = [
+//     "Wireless Internet Access",
+//     "In Room Safe",
+//     "Cable & Pay TV Channels",
+//     "Iron and Ironing Board",
+//     "Alarm Clock",
+//     "Writing Desk and Chair",
+//     "Hair Dryer",
+//   ];
+
+
+// export const RoomModal = ({ room }: any) => {
+//   const [images, setImages] = useState<string[]>([]);
+
+//   useEffect(() => {
+//     const fetchImages = async () => {
+//       const response = await axios.get(
+//         // "http://localhost:8088/config"
+//         BACKEND_URL + "/config"
+//         // "https://swhytqcdde.execute-api.ap-northeast-1.amazonaws.com/team7/config"
+//         // "http://team7ibe.ap-northeast-1.elasticbeanstalk.com/config"
+//       );
+//       // console.log(response.data.propertyConfig.first);
+//       setImages(response.data.propertyConfig.first[room.roomTypeName]);
+//     };
+//     fetchImages();
+//   }, []); 
+
+
+// };
+
+
+
+import Box from '@mui/material/Box';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Modal from '@mui/material/Modal';
+import { CarouselComponent } from "../room-result/card/CarouselComponent";
+import './RoomModal.scss'; 
+import { FormattedMessage } from 'react-intl';
+import { BACKEND_URL } from "../../constants/Constants";
+import { RoomDetails } from '../../types/RoomDetails';
+import Carousel from 'react-material-ui-carousel';
+import User from "../../assets/user.svg";
+import Doublebed from "../../assets/doublebed.svg";
+export interface RoomModalProps {
+    // onClose: () => void;
+    setOpen:React.Dispatch<React.SetStateAction<boolean>>;
+    room: RoomDetails;
+}
+
+const amenitiesList = [
+    "Wireless Internet Access",
+    "In Room Safe",
+    "Cable & Pay TV Channels",
+    "Iron and Ironing Board",
+    "Alarm Clock",
+    "Writing Desk and Chair",
+    "Hair Dryer",
+  ];
+
+export function RoomModal({ setOpen, room }: RoomModalProps) {
+    
+      const [images, setImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const response = await axios.get(
+        // "http://localhost:8088/config"
+        BACKEND_URL + "/config"
+        // "https://swhytqcdde.execute-api.ap-northeast-1.amazonaws.com/team7/config"
+        // "http://team7ibe.ap-northeast-1.elasticbeanstalk.com/config"
+      );
+      // console.log(response.data.propertyConfig.first);
+      setImages(response.data.propertyConfig.first[room.roomTypeName]);
+    };
+    fetchImages();
+  }, []); 
+    
+    
+    const handleClose = () => {
+        // onClose();
+        setOpen(false);
+
+    };
+
+
+
+
+    return (
+       <>
+            <Box className="room-modal">
+                <div className="image-carousel">
+               
+        <div
+          className="carousel-container"
+          style={{ minHeight: "235px", width: "100%" }}
+        >
+          {
+            <>
+              <Carousel animation="slide" className="carousel">
+                {images.map((image, index) => (
+                  <img
+                    key={index}
+                    src={image}
+                    alt="room"
+                    className="carousel-image"
+                    width={"100%"}
+                    height={"500px"}
+                  />
+                ))}
+              </Carousel>
+              <p className="para-carousel">{room.roomTypeName} ROOM</p>
+            </>
+          }
+        </div>
+
+                    <div className="room-title">{room.roomTypeName}</div>
+                    <button className="close-modal-btn" onClick={handleClose}>
+                        X
+                    </button>
+                </div>
+                <div className="modal-content">
+                    <div className="room-info">
+                        <div className="room-description-container">
+                            <div className='room-amenities'>
+                                <div className='room-information'>
+                                    <div className="room-capacity">
+                                        <div className="room-info-div">
+                                        <img src={User} alt="user" />
+                                        <span>&nbsp; 1-{room.maxCapacity}</span>
+                                        </div>
+                                        <div className="room-info-div">
+                                        <img src={Doublebed} alt="bed" />
+                                        {/* <span>
+                                            &nbsp; {room.doubleBed > 0 && room.doubleBed}{" "}
+                                            {room.doubleBed > 0 && (
+                                            <FormattedMessage id="king" defaultMessage="King" />
+                                            )}
+                                            {room.doubleBed > 0 && room.singleBed > 0 && " & "}
+                                            {room.singleBed > 0 && room.singleBed}{" "}
+                                            {room.singleBed > 0 && (
+                                            <FormattedMessage id="queen" defaultMessage="Queen" />
+                                            )}
+                                        </span> */}
+                                        </div>
+                                        <div className="room-info-div">
+                                        &nbsp; &nbsp;{room.area} ft²
+                                        </div>
+                                    </div>
+                                    <div className="description">
+                                        Smoke free and decorated in contemporary jewel and earth tones,
+                                        the 15-story Casino Tower rooms are located directly above the
+                                        casino. The 364 sq.ft. Casino Tower rooms are appointed with
+                                        classic furnishings and include pillow-top mattresses, 40 inch
+                                        flat panel plasma TV and Wi-Fi internet access.
+                                    </div>
+                                </div>
+                                <div className="amenities-container">
+                                    <span className="title">Amenities</span>
+                                    <div className="amenities">
+                                        {amenitiesList.map((amenity) => (
+                                        <div key={amenity} className="amenity">
+                                            <img src="/src/assets/checkmark.png"  style={{height:"16px", width:"16px"}} alt="amenities" />
+                                            <span>&nbsp;{amenity}</span>
+                                        </div>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="standard-rates">
+                                <div className='rate-title'>
+                                        Standard Rates
+                                </div>
+                                <div className='rate-card'>
+                                        <div className="rate-description">
+                                            <div className='description-title'>
+                                                Standard Rate
+                                            </div>
+                                            <div className='description-data'>
+                                                Spend $10 every night you stay and earn $150 in dining credit at the resort.
+                                            </div>
+                                        </div>
+                                        <div className="rate">
+                                            <div className='nightly-rate'>
+                                                <div className='value'>
+                                                    ${room.avgPrice}
+                                                </div>
+                                                <p className='per-night'>per night</p>
+                                            </div>
+                                            <button className='select-btn'>
+                                                SELECT PACKAGE
+                                            </button>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="standard-rates">
+                                <div className='rate-title'>
+                                        Deals & Packages
+                                </div>
+                                <div className='rate-card'>
+                                        <div className="rate-description">
+                                            <div className='description-title'>
+                                                Standard Rate
+                                            </div>
+                                            <div className='description-data'>
+                                                Spend $10 every night you stay and earn $150 in dining credit at the resort.
+                                            </div>
+                                        </div>
+                                        <div className="rate">
+                                            <div className='nightly-rate'>
+                                                <div className='value'>
+                                                    ${room.avgPrice}
+                                                </div>
+                                                <p className='per-night'>per night</p>
+                                            </div>
+                                            <button className='select-btn'>
+                                                SELECT PACKAGE
+                                            </button>
+                                        </div>
+                                </div>
+                                <div className='rate-card'>
+                                        <div className="rate-description">
+                                            <div className='description-title'>
+                                                Standard Rate
+                                            </div>
+                                            <div className='description-data'>
+                                                Spend $10 every night you stay and earn $150 in dining credit at the resort.
+                                            </div>
+                                        </div>
+                                        <div className="rate">
+                                            <div className='nightly-rate'>
+                                                <div className='value'>
+                                                    ${room.avgPrice}
+                                                </div>
+                                                <p className='per-night'>per night</p>
+                                            </div>
+                                            <button className='select-btn'>
+                                                SELECT PACKAGE
+                                            </button>
+                                        </div>
+                                </div>
+                                <div className='rate-card'>
+                                        <div className="rate-description">
+                                            <div className='description-title'>
+                                                Standard Rate
+                                            </div>
+                                            <div className='description-data'>
+                                                Spend $10 every night you stay and earn $150 in dining credit at the resort.
+                                            </div>
+                                        </div>
+                                        <div className="rate">
+                                            <div className='nightly-rate'>
+                                                <div className='value'>
+                                                    ${room.avgPrice}
+                                                </div>
+                                                <p className='per-night'>per night</p>
+                                            </div>
+                                            <button className='select-btn'>
+                                                SELECT PACKAGE
+                                            </button>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className='promocode-apply'>
+                                <div className='promocode-name'>
+                                    <p className='promo-title'>
+                                        Enter a promocode
+                                    </p>
+                                    <div className='promocode-button'>
+                                        <input className='promocode-input' type='text' />
+                                        <button className='apply-btn'>
+                                            APPLY
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Box>
+        </>
+    );
+}
