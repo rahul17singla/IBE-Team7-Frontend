0ec6dd810edd0a2dc47884372b44ce8afa91f537
arunain123 <arunainm@gmail.com>
2024-04-14 23:28:34 +0530

    displayed my bookings

--- ibe-team7-frontend/src/components/mybookings/MyBookings.tsx
+++ ibe-team7-frontend/src/components/mybookings/MyBookings.tsx
@@ -2,37 +2,36 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import { useEffect, useState } from "react";
 import "./MyBookings.scss";
-import { BACKEND_URL } from "../../constants/Constants";
+import { BACKEND_URL, FRONTEND_URL } from "../../constants/Constants";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export const MyBookings = () => {
-    const email = useSelector((state: RootState) => state.user.email);
-    const navigate = useNavigate();
+  const email = useSelector((state: RootState) => state.user.email);
+  const navigate = useNavigate();
 
-    const [bookings, setBookings] = useState([
-        { id: 1, title: "Hotel XYZ", date: "2024-04-15", location: "New York" },
-        { id: 2, title: "Hotel ABC", date: "2024-05-01", location: "Paris" },
-    ]);
+  const [bookings, setBookings] = useState([]);
 
-    useEffect(() => {
-        console.log({ email });
-        const fetchBookings = async () => {
-            const response = await axios.get(
-                `${BACKEND_URL}/api/v1/mybookings`,
-                {
-                    params: { emailId: email },
-                }
-            );
-            const data = await response.data;
-            console.log(data);
-        };
+  useEffect(() => {
+    console.log({ email });
+    const fetchBookings = async () => {
+      const response = await axios.get(`${BACKEND_URL}/api/v1/mybookings`, {
+        params: { emailId: email },
+      });
+      const data = await response.data;
+      console.log(data);
+      setBookings(data);
+    };
 
-        fetchBookings();
-    }, [email]);
+    fetchBookings();
+  }, [email]);
 
-    return (
-        /*
+  const viewBooking = (bookingId: string) => {
+    window.location.href = `confirmation/${bookingId}`;
+  };
+
+  return (
+    /*
         <div className="my-bookings-container">
             <h2>My Bookings</h2>
             <table>
@@ -64,37 +63,42 @@ export const MyBookings = () => {
             </table>
         </div>
         */
-        email === "" ? (
-            navigate("/login")
-        ) : (
-            <div className="my-bookings-container">
-                <h2>
-                    Bookings for{" "}
-                    <span style={{ color: "#26266d" }}>{email}</span>
-                </h2>
+    email === "" ? (
+      navigate("/login")
+    ) : (
+      <div className="my-bookings-container">
+        <h2>
+          Bookings for <span style={{ color: "#26266d" }}>{email}</span>
+        </h2>
 
-                <br />
-                <div className="booking-list">
-                    {bookings.map((booking) => (
-                        <div key={booking.id} className="booking">
-                            <div className="booking-details">
-                                <div className="booking-info">
-                                    <span>Title:</span> {booking.title}
-                                </div>
-                                <div className="booking-info">
-                                    <span>Date:</span> {booking.date}
-                                </div>
-                                <div className="booking-info">
-                                    <span>Location:</span> {booking.location}
-                                </div>
-                            </div>
-                            <button className="view-booking">
-                                View Bookings
-                            </button>
-                        </div>
-                    ))}
+        <br />
+        <div className="booking-list">
+          {bookings.map((booking: any) => (
+            <div key={booking.bookingMapperId} className="booking">
+              <div className="booking-details">
+                <div className="booking-info">
+                  <span>Booking Id:</span> {booking.bookingMapperId}
                 </div>
+                <div className="booking-info">
+                  <span>Full Name:</span> {booking.firstName} {booking.lastName}
+                </div>
+                <div className="booking-info">
+                  <span>Start Date:</span> {booking.startDate.split("T")[0]}
+                </div>
+                <div className="booking-info">
+                  <span>End Date:</span> {booking.endDate.split("T")[0]}
+                </div>
+              </div>
+              <button
+                className="view-booking"
+                onClick={() => viewBooking(booking.bookingMapperId)}
+              >
+                View Booking
+              </button>
             </div>
-        )
-    );
+          ))}
+        </div>
+      </div>
+    )
+  );
 };
--- ibe-team7-frontend/src/constants/Constants.ts
+++ ibe-team7-frontend/src/constants/Constants.ts
@@ -1,11 +1,10 @@
-export const BACKEND_URL =
-    // "http://localhost:8088";
-    // "https://swhytqcdde.execute-api.ap-northeast-1.amazonaws.com/team7";
-    "https://tcxv7qiee1.execute-api.ap-northeast-1.amazonaws.com/test7";
+// export const BACKEND_URL = "http://localhost:8088";
+// "https://swhytqcdde.execute-api.ap-northeast-1.amazonaws.com/team7";
+"https://tcxv7qiee1.execute-api.ap-northeast-1.amazonaws.com/test7";
 
 export const FRONTEND_URL = "https://sprint-5.d1uu6vl2t1u60t.amplifyapp.com";
 // export const FRONTEND_URL = "http://localhost:5173";
 
 export const LARGE_DATE = "2050-12-31";
 export const GEO_API_KEY =
-    "ZU5xWllnV2hTU0syY0FqVjIyUFNlamt3djBXOTBWZFVLYjlCNnp5eQ==";
+  "ZU5xWllnV2hTU0syY0FqVjIyUFNlamt3djBXOTBWZFVLYjlCNnp5eQ==";
