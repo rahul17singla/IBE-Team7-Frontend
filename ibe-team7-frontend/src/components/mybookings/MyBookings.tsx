b0c4f4a60fdcfc3368e6ff20509c365c23a39930
rahul17singla <rahulsingla1710@gmail.com>
2024-04-15 11:00:29 +0530

    google analytics script added

--- ibe-team7-frontend/src/components/mybookings/MyBookings.tsx
+++ ibe-team7-frontend/src/components/mybookings/MyBookings.tsx
@@ -7,31 +7,34 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export const MyBookings = () => {
-  const email = useSelector((state: RootState) => state.user.email);
-  const navigate = useNavigate();
+    const email = useSelector((state: RootState) => state.user.email);
+    const navigate = useNavigate();
 
-  const [bookings, setBookings] = useState([]);
+    const [bookings, setBookings] = useState([]);
 
-  useEffect(() => {
-    console.log({ email });
-    const fetchBookings = async () => {
-      const response = await axios.get(`${BACKEND_URL}/api/v1/mybookings`, {
-        params: { emailId: email },
-      });
-      const data = await response.data;
-      console.log(data);
-      setBookings(data);
+    useEffect(() => {
+        console.log({ email });
+        const fetchBookings = async () => {
+            const response = await axios.get(
+                `${BACKEND_URL}/api/v1/mybookings`,
+                {
+                    params: { emailId: email },
+                }
+            );
+            const data = await response.data;
+            console.log(data);
+            setBookings(data);
+        };
+
+        fetchBookings();
+    }, [email]);
+
+    const viewBooking = (bookingId: string) => {
+        window.location.href = `confirmation/${bookingId}`;
     };
 
-    fetchBookings();
-  }, [email]);
-
-  const viewBooking = (bookingId: string) => {
-    window.location.href = `confirmation/${bookingId}`;
-  };
-
-  return (
-    /*
+    return (
+        /*
         <div className="my-bookings-container">
             <h2>My Bookings</h2>
             <table>
@@ -63,42 +66,49 @@ export const MyBookings = () => {
             </table>
         </div>
         */
-    email === "" ? (
-      navigate("/login")
-    ) : (
-      <div className="my-bookings-container">
-        <h2>
-          Bookings for <span style={{ color: "#26266d" }}>{email}</span>
-        </h2>
+        email === "" ? (
+            navigate("/login")
+        ) : (
+            <div className="my-bookings-container">
+                <h2>
+                    Bookings for{" "}
+                    <span style={{ color: "#26266d" }}>{email}</span>
+                </h2>
 
-        <br />
-        <div className="booking-list">
-          {bookings.map((booking: any) => (
-            <div key={booking.bookingMapperId} className="booking">
-              <div className="booking-details">
-                <div className="booking-info">
-                  <span>Booking Id:</span> {booking.bookingMapperId}
+                <br />
+                <div className="booking-list">
+                    {bookings.map((booking: any) => (
+                        <div key={booking.bookingMapperId} className="booking">
+                            <div className="booking-details">
+                                <div className="booking-info">
+                                    <span>Booking Id:</span>{" "}
+                                    {booking.bookingMapperId}
+                                </div>
+                                <div className="booking-info">
+                                    <span>Full Name:</span> {booking.firstName}{" "}
+                                    {booking.lastName}
+                                </div>
+                                <div className="booking-info">
+                                    <span>Start Date:</span>{" "}
+                                    {booking.startDate.split("T")[0]}
+                                </div>
+                                <div className="booking-info">
+                                    <span>End Date:</span>{" "}
+                                    {booking.endDate.split("T")[0]}
+                                </div>
+                            </div>
+                            <button
+                                className="view-booking"
+                                onClick={() =>
+                                    viewBooking(booking.bookingMapperId)
+                                }
+                            >
+                                View Booking
+                            </button>
+                        </div>
+                    ))}
                 </div>
-                <div className="booking-info">
-                  <span>Full Name:</span> {booking.firstName} {booking.lastName}
-                </div>
-                <div className="booking-info">
-                  <span>Start Date:</span> {booking.startDate.split("T")[0]}
-                </div>
-                <div className="booking-info">
-                  <span>End Date:</span> {booking.endDate.split("T")[0]}
-                </div>
-              </div>
-              <button
-                className="view-booking"
-                onClick={() => viewBooking(booking.bookingMapperId)}
-              >
-                View Booking
-              </button>
             </div>
-          ))}
-        </div>
-      </div>
-    )
-  );
+        )
+    );
 };
